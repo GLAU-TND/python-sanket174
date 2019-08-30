caac47419f9e0bf5df4179f8d786ae76efa6e4a7
sanket174 <sanketkumar174@gmail.com>
2019-08-30 18:03:01 +0530

    test case pass

--- bottlenecksolution.py
+++ bottlenecksolution.py
@@ -1,17 +1,26 @@
-c=int(input("enter the no.of element"))
+'''c=int(input("enter the no.of element"))
 a=list(map(int,input().split( )))
 i=0
-a[i].sort()
+a.sort()
 x = 0
-c=a[1]
-if c.equals(a[i]):
+a[1]=c
+if c==(a[i]):
   for i in range(1):
 
        x=x+1
        print(x)
 else:
        print(1)
+'''
 
 
 
+n=int(input("enter the no."))
+res=list(map(int,input().split()))
+lst=[]
+
+for i in res:
+    lst.append(res.count(i))
+
+print(max(lst))
 
