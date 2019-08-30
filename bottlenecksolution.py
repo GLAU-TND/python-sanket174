dacc6fe88f1ec8c6cc1d48e7e4e77e43acba7f1f
sanket174 <sanketkumar174@gmail.com>
2019-08-30 17:50:04 +0530

    input given ,1-test case fails

---
+++ bottlenecksolution.py
@@ -0,0 +1,17 @@
+c=int(input("enter the no.of element"))
+a=list(map(int,input().split( )))
+i=0
+a[i].sort()
+x = 0
+c=a[1]
+if c.equals(a[i]):
+  for i in range(1):
+
+       x=x+1
+       print(x)
+else:
+       print(1)
+
+
+
+
